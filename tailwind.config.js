c82f171d51382c40523d9b168f0e566593346702
Michael Tong <michaelyuyangtong@gmail.com>
2022-02-04 13:57:13 +0800

    Configured tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: colors.zinc,
+        accent: ({ opacityValue }) => {
+          if (opacityValue !== undefined) {
+            return `rgba(var(--accent), ${opacityValue})`;
+          }
+          return `rgb(var(--accent)`;
+        },
+      },
+      screens: {
+        xs: "480px",
+        ...defaultTheme.screens,
+      },
+    },
   },
   plugins: [],
 };
